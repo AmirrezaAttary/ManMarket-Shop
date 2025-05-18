74adaa9b4df7ffcedd57589d2d1fa9209f42f1ca
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-05-18 10:53:09 -0700

    change static to staticfiles

--- core/core/settings.py
+++ core/core/settings.py
@@ -148,14 +148,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS =  [
-    BASE_DIR / 'static'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
 ]
 
 # Default primary key field type
