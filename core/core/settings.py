0ead2bd1f5ca9f6c63ccb4bc75c944fcdb002be8
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-05-28 11:14:47 -0700

    Add password reset functionality with corresponding templates
    - Created password reset HTML templates: password_reset.html, password_reset_complete.html, password_reset_confirm.html, password_reset_done.html.
    - Implemented email templates for password reset: password_reset_email.tpl and password_reset_subject.txt.
    - Added Persian language support for user prompts and messages throughout the password reset process.

--- core/core/settings.py
+++ core/core/settings.py
@@ -167,13 +167,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = config("EMAIL_HOST",default="smtp4dev")
+# EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool ,default=False)
+# EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool ,default=False)
+# EMAIL_PORT = config("EMAIL_PORT", cast=int,default=25)
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER",default="")
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD",default="")
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST",default="smtp4dev")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool ,default=False)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool ,default=False)
-EMAIL_PORT = config("EMAIL_PORT", cast=int,default=25)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER",default="")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD",default="")
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "tarya0039@gmail.com"
+EMAIL_HOST_PASSWORD = "alzw ugjy kdzu yurh"
 
 
 
