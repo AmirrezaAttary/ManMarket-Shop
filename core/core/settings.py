c3f0f6f147f65cc86de04ed6e21de4df1fba050a
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-06-11 08:01:11 -0700

    Comment out cart_processor context processor in settings for future adjustments

--- core/core/settings.py
+++ core/core/settings.py
@@ -124,7 +124,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processors.cart_processor',
+                # 'cart.context_processors.cart_processor',
                 # 'shop.context_processors.wishlist_total_items',
             ],
         },
