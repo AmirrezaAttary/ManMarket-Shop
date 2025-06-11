f97b35c14fcc1f97bae37038dd084db8a48c3c91
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-06-11 07:58:46 -0700

    Comment out wishlist_total_items context processor in templates for future use

--- core/core/settings.py
+++ core/core/settings.py
@@ -125,7 +125,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart_processor',
-                'shop.context_processors.wishlist_total_items',
+                # 'shop.context_processors.wishlist_total_items',
             ],
         },
     },
