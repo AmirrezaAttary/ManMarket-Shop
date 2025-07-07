386764cc00a9b188e10157b3820a6eea9ccb5b4c
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-07-07 15:42:00 +0330

    set template tag in shop

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -19,14 +19,17 @@ def show_latest_products(context):
         "wishlist_items": wishlist_items
     }
 
+
+
 @register.inclusion_tag("includes/highest-discount-products.html", takes_context=True)
 def show_highest_discount_products(context):
     request = context.get("request")
     highest_discount_products = ProductModel.objects.filter(
-        status=ProductStatusType.publish.value
+        status=ProductStatusType.publish.value,
+        color_inventories__price__gt=0  # فیلتر محصولات دارای قیمت مثبت
     ).annotate(
         discount=F('color_inventories__price') * (F('color_inventories__discount_percent') / 100)
-    ).order_by('-discount')[:16]
+    ).order_by('-discount').distinct()[:16]  # distinct برای جلوگیری از تکرار محصول به‌خاطر رابطه many-to-one
 
     wishlist_items = WishlistProductModel.objects.filter(user=request.user).values_list("product__id", flat=True) if request.user.is_authenticated else []
 
@@ -36,12 +39,14 @@ def show_highest_discount_products(context):
         "wishlist_items": wishlist_items
     }
 
+
 @register.inclusion_tag("includes/highest-sales-products.html", takes_context=True)
 def show_highest_sales_products(context):
     request = context.get("request")
     highest_sales_products = ProductModel.objects.filter(
-        status=ProductStatusType.publish.value
-    ).order_by('-sales_count')[:16]
+        status=ProductStatusType.publish.value,
+        color_inventories__price__gt=0
+    ).order_by('-sales_count').distinct()[:16]
 
     wishlist_items = WishlistProductModel.objects.filter(user=request.user).values_list("product__id", flat=True) if request.user.is_authenticated else []
 
@@ -51,12 +56,15 @@ def show_highest_sales_products(context):
         "wishlist_items": wishlist_items
     }
 
+
+
 @register.inclusion_tag("includes/most-viewed-products.html", takes_context=True)
 def show_most_viewed_products(context):
     request = context.get("request")
     most_viewed_products = ProductModel.objects.filter(
-        status=ProductStatusType.publish.value
-    ).order_by('-product_view')[:16]
+        status=ProductStatusType.publish.value,
+        color_inventories__price__gt=0
+    ).order_by('-product_view').distinct()[:16]
 
     wishlist_items = WishlistProductModel.objects.filter(user=request.user).values_list("product__id", flat=True) if request.user.is_authenticated else []
 
