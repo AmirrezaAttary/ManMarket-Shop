8e9f94ebeef5be4e2db995f3e2ad2f92ea205b2e
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-07-16 16:58:43 +0330

    set setting

---
+++ core/core/config/prod.py
@@ -0,0 +1,25 @@
+from core.settings import *
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY",default='django-insecure-jc35qo)gv(idt+zlq=2&!yq%y)twxum-$8@s^#3dj^7n^!yn_e')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+ALLOWED_HOSTS = [
+    '192.168.73.237',
+    '127.0.0.1',
+    'manmarket.ir',
+    'www.manmarket.ir',
+    'mail.manmarket.ir',  # ← این خط را اضافه کن
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://manmarket.ir",
+    "https://www.manmarket.ir",
+    "https://mail.manmarket.ir",# اگر از www هم استفاده می‌کنی
+]
+"""https://api.digikala.com/v1/rate-review/products/18132558/"""
--- core/core/settings.py
+++ core/core/settings.py
@@ -23,29 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY",default='django-insecure-jc35qo)gv(idt+zlq=2&!yq%y)twxum-$8@s^#3dj^7n^!yn_e')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 
-ALLOWED_HOSTS = [
-    '192.168.73.237',
-    '127.0.0.1',
-    'manmarket.ir',
-    'www.manmarket.ir',
-    'mail.manmarket.ir',  # ← این خط را اضافه کن
-]
-
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://manmarket.ir",
-    "https://www.manmarket.ir",
-    "https://mail.manmarket.ir",# اگر از www هم استفاده می‌کنی
-]
-"""https://api.digikala.com/v1/rate-review/products/18132558/"""
 
 # Application definition
 
