04515baa86e7469ab92afa7d3c221b619b386e9a
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-07-20 17:16:36 +0330

    change base file

--- core/shop/models/megamenu.py
+++ core/shop/models/megamenu.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class MegaMenu(models.Model):
     category = models.ForeignKey("shop.ProductCategoryModel", on_delete=models.CASCADE, related_name="mega_menus")
-    brand = models.ForeignKey("shop.Brand", on_delete=models.CASCADE, null=True, blank=True)
+    brand = models.ForeignKey("shop.Brand", on_delete=models.CASCADE)
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
