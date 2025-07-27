b5e968a382d0b831bd09a2b0479b106c5c3a2431
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-07-27 17:03:26 +0330

    Add slug field to blog category forms and templates

--- core/dashboard/admin/forms/blog_category.py
+++ core/dashboard/admin/forms/blog_category.py
@@ -7,11 +7,13 @@ class BlogCategoryModelForm(forms.ModelForm):
         model = Category
         fields = [
             'name',
+            'slug'
         ] 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['slug'].widget.attrs['class'] = 'form-control'
 
 
 
