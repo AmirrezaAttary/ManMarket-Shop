a4d643f95718b8c409da135359be490adb0f840b
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-07-29 17:34:01 +0330

    Remove unique constraint from url field in PriceSpecification model

--- core/getspecification/models.py
+++ core/getspecification/models.py
@@ -6,7 +6,7 @@ class PriceSpecification(models.Model):
     Model representing a price get digikala.
     """
     product = models.OneToOneField('shop.ProductModel', null=True, blank=True, on_delete=models.CASCADE, related_name='pricespecification')
-    url = models.URLField(max_length=255,null=True,blank=True, unique=True)
+    url = models.URLField(max_length=255,null=True,blank=True)
 
 
     created_date = models.DateTimeField(auto_now_add=True)
