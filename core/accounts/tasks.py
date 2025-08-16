2592de6621953a0c1ff102f26297a1825328451f
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-08-16 11:34:45 +0330

    Add task to delete unverified users after 20 minutes; enhance OTP management

--- core/accounts/tasks.py
+++ core/accounts/tasks.py
@@ -1,7 +1,24 @@
 # accounts/tasks.py
 from celery import shared_task
 from .models import EmailOTP
+from django.utils import timezone
+from datetime import timedelta
+from accounts.models import User
+
 
 @shared_task
 def clean_expired_otps():
     EmailOTP.delete_expired()
+
+
+@shared_task
+def delete_unverified_users():
+    cutoff = timezone.now() - timedelta(minutes=20)
+    users = User.objects.filter(
+        created_date__lt=cutoff,
+        is_verified=False,
+        is_phone_verified=False
+    )
+    count = users.count()
+    users.delete()
+    return f"{count} unverified users deleted."
