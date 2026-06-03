6ddc8ad57c5615d32323d807dfc065470e351279
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2026-06-03 19:05:09 +0330

    Update wishlist.py

--- core/app/dashboard/api/v1/user/serializers/wishlist.py
+++ core/app/dashboard/api/v1/user/serializers/wishlist.py
@@ -2,21 +2,23 @@ from rest_framework import serializers
 from app.shop.models import WishlistProductModel
 
 class WishlistProductModelSerializers(serializers.ModelSerializer):
-    
+
     absolute_url = serializers.SerializerMethodField()
+    product_slug = serializers.CharField(source='product.slug', read_only=True)
+
     class Meta:
         model = WishlistProductModel
         fields = '__all__'
 
-
     def to_representation(self, instance):
         request = self.context.get("request")
         rep = super().to_representation(instance)
-        if request.parser_context.get("kwargs").get("pk"):
+
+        if request and request.parser_context.get("kwargs", {}).get("pk"):
             rep.pop("absolute_url", None)
 
         return rep
-        
+
     def get_absolute_url(self, obj):
         request = self.context.get("request")
         return f"{request.build_absolute_uri(obj.pk)}/"
